f55b7c8f27bad33880c7baf593f334cb926740e4
seamys1313 <faiblefemme@gmail.com>
2024-08-14 03:02:06 +0300

    На страницу добавлен баннер

--- src/components/BannerOnePage.vue
+++ src/components/BannerOnePage.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="banner">
-        <img src="/vue-project/src/assets/LogoO.png" alt="image">
+        <img class="banner-image" src="../assets/Banner_One.png" alt="image">
     </div>
 </template>
 
@@ -13,5 +13,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+    .banner-image {
+        width: 930px;
+        height: 550px;
+        margin-top: 20px
+    }
 </style>
